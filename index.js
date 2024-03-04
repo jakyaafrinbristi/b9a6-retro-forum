8ac1f28a82e151eb1065adf3068c9f596cd60a6f
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-03-04 20:52:29 +0600

    discuss section added

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-
-
+let total = 0;
 const loadData = async () => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
-    const data= await res.json();
-    displayData(data.posts);
-    
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+  const data = await res.json();
+  displayData(data.posts);
+
 
 }
-const cardContainer=document.getElementById("discuss-container");
-const displayData =(cards)=>{
-    // console.log(cards)
-  
-    cards.forEach(card=>{
-        // console.log(card)
-        const dataCard = document.createElement('div');
-        dataCard.classList=`card card-side bg-base-100 shadow-xl`
-        dataCard.innerHTML=`
-       <div> 
-       
-       <figure><img class="w-20 px-2 py-5 rounded-2xl" src="${card.image}"/></figure>
+const cardContainer = document.getElementById("discuss-container");
+
+const displayData = (cards) => {
+  // console.log(cards)
+
+  cards.forEach(card => {
+    // console.log(card)
+    const dataCard = document.createElement('div');
+    dataCard.classList = `card card-side bg-base-100 shadow-xl`
+    dataCard.innerHTML = `
+      <div>  <figure><img class="w-20 px-2 py-5 rounded-2xl" src="${card.image}"/></figure>
+      </div>
+      
        </div>
         <div class="card-body">
          <div class="flex gap-3">
@@ -42,29 +42,70 @@ const displayData =(cards)=>{
           <p>${card.posted_time}</p>
           </div>
           </div>
-          <div id="button"class="card-actions justify-end">
+          <div class="button card-actions justify-end" onclick="buttonData(${card.id})">
           <img src="./images/message.png" alt="">
           </div>
         </div>
         `
-        cardContainer.appendChild(dataCard)
- 
-    });
+    cardContainer.appendChild(dataCard)
+
+  });
+
 
 }
-const loadCard = async()=>{
-    const res=await fetch(` https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
-    const data = await res.json()
-    displayCard(data)
+const markRead = document.getElementById("read")
+
+const button = document.getElementById("tbody-container")
+const buttonData = async (id) => {
+  // console.log(id)
+  total++;
+  markRead.innerText = total
+
+  const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`)
+  const data = await res.json()
+  // console.log(data)
+  const posts = data?.posts
+  // console.log(posts)
+  const singlePost = posts.find(post => post.id === id)
+  console.log(singlePost)
+  const postElement = document.createElement("div");
+
+  postElement.classList = `card  bg-base-100 shadow-xl 	`
+  postElement.innerHTML = `<div class="card-body">
+  <div class="flex justify-between items-center gap-10">
+  <h2 class="font-bold">${singlePost.title}</h2>
+  <div  class="font-bold"><i class="fa-regular fa-eye"></i></div>
+  <p  class="font-bold">${singlePost.view_count}</p></div>
+  
+</div>
+  
+  
+  `
+
+  button.appendChild(postElement)
+
+
+
+
+
 }
-const latestCard=document.getElementById("latest-post-container");
-const displayCard=(allCard)=>{
-    // console.log(allCard)
-    allCard.forEach(card=>{
-    console.log(card)
-    const cardDiv=document.createElement('div');
-    cardDiv.classList=`card  bg-base-100 shadow-xl`
-    cardDiv.innerHTML=`<figure class="px-10 pt-10">
+
+
+
+const loadCard = async (id) => {
+  const res = await fetch(` https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
+  const data = await res.json()
+  displayCard(data)
+
+}
+const latestCard = document.getElementById("latest-post-container");
+const displayCard = (allCard) => {
+  // console.log(allCard)
+  allCard.forEach(card => {
+    // console.log(card)
+    const cardDiv = document.createElement('div');
+    cardDiv.classList = `card  bg-base-100 shadow-xl`
+    cardDiv.innerHTML = `<figure class="px-10 pt-10">
     <img src="${card.cover_image}" alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-start">
@@ -86,10 +127,10 @@ const displayCard=(allCard)=>{
     </div>
     
   </div>`
-  latestCard.appendChild(cardDiv)
-    })
+    latestCard.appendChild(cardDiv)
+  })
 
-    }
+}
 
 
 loadCard()
